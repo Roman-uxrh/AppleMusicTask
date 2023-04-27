92556ce15059e2e11f5de4f34c39f8323747a6f5
RRDubina <dubinaroman.dubina@gmail.com>
2023-04-28 00:37:04 +0500

    Initial Commit

---
+++ AppleMusicTask/AppleMusicTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicTaskApp.swift
+//  AppleMusicTask
+//
+//  Created by admin on 28.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
